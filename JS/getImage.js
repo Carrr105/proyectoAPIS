b7a29413f9f646b828e3bf642163855d13091aad
Jesus Lozano <jesus.javi.lozano@hotmail.com>
2021-05-25 00:22:34 -0500

    se agrego un boton para generar una imagen nueva con la funcionaldiad

---
+++ JS/getImage.js
@@ -0,0 +1,24 @@
+var after = ''
+
+function getnewimage(){
+    console.log("entro")
+    let parentdiv = document.createElement('div')
+    parentdiv.id = 'randImg'
+    fetch('https://www.reddit.com/r/memes.json')
+    .then(response => response.json())
+    .then(body =>{
+        for(let i = 0; i < body.data.children.lenght; index++){
+            if(body.data.children[index].data.post_hint == 'image'){
+                let div = document.createElement('div')
+                let h4 = document.createElement('h4')
+                let image = docuemnt.createElement('img')
+                image.src = body.data.children[index].data.url_overriden_by_dest
+                h4.textContent = body.data.children[index].data.title
+                div.appendChild(h4)
+                div.appendChild(image)
+                parentdiv.appendChild(div)
+            }
+        }
+        document.body.appendChild(parentdiv)
+    })
+}
