76c87714775d9eded7eb110660a8fec08b84e02f
Isabellacnb <canalesisabella4@gmail.com>
2021-05-26 14:30:50 -0500

    Add and update css to make project more appealing :)

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 const app = express();
 // importing routes
-const routes = require('./routes');
-
+const routes = require("./routes");
 
 // settings
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
-
-// routes
-app.use('/', routes);
-
-app.listen(app.get('port'), () =>{
-    console.log(`server on port ${app.get('port')}`);
+// css
+app.get("/css/styles.css", function (req, res) {
+  res.sendFile(__dirname + "/css/styles.css");
 });
-   
+// routes
+app.use("/public", express.static(__dirname + "public"));
+app.use("/", routes);
+
+app.listen(app.get("port"), () => {
+  console.log(`server on port ${app.get("port")}`);
+});
